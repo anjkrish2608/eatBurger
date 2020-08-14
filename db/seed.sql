a69fab3479243bd7ef44b89794e1b8f252c8278f
Anjini Krishnan <anjini.krishnan@hotmail.com>
2020-08-14 18:11:47 +0930

    basic function replicated from acitivity

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name,devoured) 
+VALUES("BLT",false),("Hamburger",true),("the biggie smalls", true);
+INSERT INTO burgers (name) 
+VALUES ('the aussie'),("the peckish"),("lil burger");
