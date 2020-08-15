6afe078464af8f2e29998f0271c78920b814f846
Anjini Krishnan <anjini.krishnan@hotmail.com>
2020-08-15 15:33:14 +0930

    changing devour status functioning

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -11,7 +11,6 @@ router.get("/", function(req, res) {
     var hbsObject = {
       burgers: data
     };
-    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
@@ -33,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    devoured: req.body.sleepy
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
